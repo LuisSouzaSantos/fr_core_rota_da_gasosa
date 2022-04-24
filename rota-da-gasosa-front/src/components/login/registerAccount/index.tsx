8c99c1756d3b0d077cca2fd1c48e41cb616c81e7
Igor Carvalho <igorcarvalhoum@hotmail.com>
2022-04-24 20:33:26 -0300

    Screen Register

--- rota-da-gasosa-front/src/components/login/registerAccount/index.tsx
+++ rota-da-gasosa-front/src/components/login/registerAccount/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import registerStyles from './Register.module.css';
-//import logo from '../assets/images/rota-icone1.png'; 
 import { Link } from "react-router-dom";
 
 
@@ -9,20 +8,31 @@ class RegisterAccount extends React.Component {
     render() {
 
         return (
-            <div className={registerStyles.loginContainer}>
+            <div className={registerStyles.registerContainer}>
                 <div className={registerStyles.formContainer}>
-
                     <div>
+                        <button className={registerStyles.buttonReturn}><Link to={"/"}>Voltar</Link></button>
+                    </div>
+                    <div className={registerStyles.register}>Cadastro</div>
+                    <div className={registerStyles.registerForm}>
                         <form>
-                            <input placeholder="Digite seu CPF/CNPJ..." type="text" required/>
+                            <label htmlFor="profile" className={registerStyles.registerProfile}>Selecione o perfil:  </label>
+                            <select name="profile" id="profile" required>
+                                <option value="registerCNPJ">CNPJ</option>
+                                <option value="registerCPF">CPF</option>
+                            </select>
+
+
+                            <input placeholder="Digite o nome completo" type="text" required/>
+                            <input placeholder="Digite sua senha..." type="text" required/>
+                            <input placeholder="Digite sua senha..." type="text" required/>
+                            <input placeholder="Digite sua senha..." type="text" required/>
                             <input placeholder="Digite sua senha..." type="password" required/>
                         </form>
                     </div>
-                    <p className={registerStyles.recoveryPasswordText}><a>Esqueci minha senha</a></p>
-                    <div >
-                        <button className={registerStyles.buttonLogin}>LOGIN</button>
+                    <div>
+                        <button className={registerStyles.buttonRegister}><Link to={"/"}>Cadastrar</Link></button>
                     </div>
-                    <p className={registerStyles.registerAccount} >Não tem conta? <Link to={"/"}>Cadastre-se</Link></p>    
                 </div>    
             </div>
         )
